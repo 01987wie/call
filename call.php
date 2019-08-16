e8cb2de60ff05606785eec4ebd60947aedba38a6
bross <53391521+01987wie@users.noreply.github.com>
2019-08-16 05:24:25 +0000

    Add files via upload

---
+++ call.php
@@ -0,0 +1,19 @@
+<?php
+// Limit 3x Telpon Setiap Satu Nomor
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_close($ch);
+                echo $asw."\n";
+}
+echo "\n\n\033[92;1m[ WIRA ]\033[91;1m [ @bangbrazzer ]\033[96;1m\n";
+echo "Nomor 628xxxx\n\033[91;1mInput :\033[97;1m ";
+$nomor = trim(fgets(STDIN));
+$execute = send($nomor);
+print $execute;
+?>
